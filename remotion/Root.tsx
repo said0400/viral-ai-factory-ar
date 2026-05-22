0483cd04f624c27ddd468991f0ab21f1d688ca64
said0400 <said.amarzoug04@gmail.com>
2026-05-22 11:56:46 +0100

    Update Root.tsx

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -1,7 +1,18 @@
 import { Composition } from 'remotion';
 import { ViralVideo } from './Composition';
 import scriptData from '../assets/script.json';
+
 export const Root: React.FC = () => {
-  const dur = scriptData.scenes.reduce((s, x) => s + x.duration, 0);
-  return (<Composition id="ViralVideo" component={ViralVideo} durationInFrames={dur * 30} fps={30} width={1080} height={1920} defaultProps={{ scenes: scriptData.scenes }} />);
+  const totalDuration = scriptData.scenes.reduce((sum, scene) => sum + scene.duration, 0);
+  return (
+    <Composition
+      id="ViralVideo"
+      component={ViralVideo}
+      durationInFrames={totalDuration * 30}
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{ scenes: scriptData.scenes }}
+    />
+  );
 };
